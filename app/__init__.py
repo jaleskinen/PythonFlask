fccf90f0af9de6d8d633148d604be91e3440fef2
jaleskinen <jarmo.leskinen@hepokumpu.com>
2016-01-27 14:37:22 +0200

    27-01-2016 Jarmo Leskinen, first python app created

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+#This lines configures our app using the config.py file
+app.config.from_object('config')
 from app import routers
--- app/routers.py
+++ app/routers.py
@@ -1,10 +1,28 @@
 """Search from app directory app module (now found from __init__.py)"""
 from app import app
+#render_template gives you access to Jinja2 template
+from flask import render_template, request, make_response
 
 #This is comment also, but you can use # only in one line comments
 
 @app.route('/')
 def index():
-    return 'Hellou World!'
+    name = 'Aku'
+    address = 'Ankkalinna'
+    response = make_response(render_template('template_index.html', title=address, name=name))
+    response.headers.add('Cache-control','no-cache')
+    response.headers.add('diipadaapa','jippii')
+    return response
 
+@app.route('/user/<name>')
+def user(name):
+    print("User-Agent: " + request.headers.get('User-Agent'))
+    return render_template('template_user.html',name=name)
+
+#Example how you can define route methods
+@app.route('/user',methods=['GET','POST'])
+def userParams():
+    name = request.args.get('name')
+    return render_template('template_user.html',name=name)
+    
 #print('This is not any more included in index() function')
